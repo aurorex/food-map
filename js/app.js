cbd5dc1f792156a67b88a8126fecb8a4e527f1db
aurorex <selva_beat@hotmail.com>
2018-01-02 03:02:46 +0100

    modificando archivos html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+/* el setTimeout direcciona a la vista principal despues de 3 segundos */
   setTimeout(function() { 
-    window.location.href = 'views/search.html';
+    window.location.href = 'views/principal.html';
   }, 3000);
 });
 
